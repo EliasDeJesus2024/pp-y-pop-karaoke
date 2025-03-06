9d8e19858a15b41de15fd5b7371eccb57639eb95
JeffersonEscobar2024 <jefferson.escobar2024@hilasal.superate.org.sv>
2025-03-06 09:38:25 -0600

    Arreglopenultimo

--- main.js
+++ main.js
@@ -7,15 +7,38 @@ const playlist = document.getElementById('play_list');
 
 function addsong(songName, artistName, songUrl) {
     const listSong = document.createElement('li');
+    listSong.classList.add(
+        'flex','justify-between','items-center','p-4','bg-white','shamd','rounded-lg','mb-4','border','border-gray-300','hover:bg-gray-100' 
+    ,);
+
+
     listSong.innerHTML = `
+    <span class="font-semibold text-lg text-gray-800">
     <strong>${songName}</strong>
     - ${artistName}
-    <a href="${songUrl}" target="_blank" >Reproducir</a>
-    <button class="delete-btn">Eliminar </button>
+    <div class="flex space-x-4">
+    <a href="${songUrl}" target="_blank" 
+    class="text-blue-500 hover:text-blue-700 transition-colors duration-300 
+    px-3 py-2 rounded-lg border border-blue-500 hover:bg-blue-500 hover:text-white">
+    Reproducir</a>
 
+    <button  class="delete-btn text-red-500 hover:text-red-700 transition-colors duration-300 
+                px-3 py-2 rounded-lg border border-red-500 hover:bg-red-500 hover:text-white">
+                Eliminar 
+                </button>  
+            
+    
+                </div>
+    
     `;
 //agregar al html
     playlist.appendChild(listSong);
+
+    // Agregar evento para eliminar la canción
+    const deleteButton = listSong.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', () => {
+        listSong.remove();  // Elimina la canción de la lista
+    });
 
 
 }    
